3435f7d927ed11ebc943921ec3cb687deaeebf20
tarun-kataruka <tarunkataruka22@gmail.com>
2025-05-14 20:18:31 +0530

    Update download link for resume PDF file

--- app/page.js
+++ app/page.js
@@ -34,8 +34,8 @@ const Page = () => {
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <a
-                href="/Tarun.pdf"
-                download="Tarun.pdf"
+                href="/DL.pdf"
+                download="DL.pdf"
               >
                 <Button
                   variant="outline"

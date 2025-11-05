fd018f77c45a82b9be501439c173550c7d4c1088
tarun-kataruka <tarunkataruka22@gmail.com>
2025-11-05 18:05:30 +0530

    fix: update download link for CS Lab PDF file

--- app/page.js
+++ app/page.js
@@ -33,8 +33,8 @@ const Page = () => {
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8 ml-12">
               <a
-                href="/TarunKataruka_1DS22CS232.pdf"
-                download="TarunKataruka.pdf"
+                href="/CS_Lab.pdf"
+                download="123.pdf"
               >
                 <Button
                   variant="outline"
